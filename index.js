3af8b0470f3cc997ec3bee7b66b7c53fc36d2022
nobody <73385158+dikanata@users.noreply.github.com>
2020-12-17 17:03:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 const { Client } = require('whatsapp-web.js');
 const fs = require('fs-extra');
 
-const SESSION_FILE_PATH = './session.json';
-let sessionCfg;
-if (fs.existsSync(SESSION_FILE_PATH)) {
-    sessionCfg = require(SESSION_FILE_PATH);
-}
 
 const client = new Client({ puppeteer: { headless: true, 
     args: [
     '--no-sandbox'
 ], 
-session: sessionCfg
-
 }});
 
 client.on('qr', (qr) => {
     console.log(qr);
-});
-
-client.on('authenticated', (session) => {
-    console.log('Berhasil login!');
-    sessionCfg=session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-        if (err) {
-            console.error(err);
-        }
-    });
 });
 
 client.on('ready', () => {
