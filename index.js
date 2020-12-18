1fc58e17c4d6898483004e3bd423925beaeff74d
nobody <73385158+dikanata@users.noreply.github.com>
2020-12-18 16:25:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,12 @@ const { Client } = require('whatsapp-web.js');
 const fs = require('fs');
 const fse = require('fs-extra');
 
-const SESSION_FILE_PATH = './session.json';
-let sessionCfg;
-if (fs.existsSync(SESSION_FILE_PATH)) {
-    sessionCfg = require(SESSION_FILE_PATH);
-}
-
 const client = new Client({ puppeteer: { headless: true,
     args: [
         "--no-sandbox"
       ]
 
-}, session: sessionCfg });
+}});
 
 client.initialize();
 
@@ -21,18 +15,12 @@ client.on('qr', (qr) => {
     console.log(qr);
 });
 
-client.on('authenticated', (session) => {
-    console.log('Berhasil diauntentikasi!', session);
-    sessionCfg=session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-        if (err) {
-            console.error(err);
-        }
-    });
+client.on('authenticated', () => {
+    console.log('Berhasil diauntentikasi!');
 });
 
-client.on('auth_failure', msg => {
-    console.error('Autentikasi gagal!', msg);
+client.on('auth_failure', () => {
+    console.error('Autentikasi gagal!');
 });
 
 client.on('ready', () => {
@@ -120,7 +108,7 @@ Contoh : !demote @sadbot
     
     else if (msg.body == '!turnoff'){
         if (msg.from == '6285841392048@c.us' || msg.author.includes('6285841392048')){
-            client.destroy();
+            client.logout();
         } else {
             msg.reply('Maaf kamu bukan pemilik bot ini!');
         }
