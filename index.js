9c01e15fb148977925894b82a8196d7515e3b605
nobody <73385158+dikanata@users.noreply.github.com>
2020-12-18 16:33:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -117,8 +117,7 @@ Contoh : !demote @sadbot
 
 client.on('group_join', async (notification) => {
     let number = await notification.id.remote;
-    client.sendMessage(number, `Ada member baru nih`);
-    client.sendMessage(msg.from, `Halo selamat datang di grup ${chat.name}, Jangan lupa baca deskripsi grup ya!`);
+    client.sendMessage(number, `Halo selamat datang di grup ${chat.name}, Jangan lupa baca deskripsi grup ya!`);
 });
 
 client.on('group_leave', async (notification) => {
