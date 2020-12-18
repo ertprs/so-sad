598eafc5bc4fcd9cddc681af1f7481ca0b7ce2e5
nobody <73385158+dikanata@users.noreply.github.com>
2020-12-18 14:52:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,75 @@
 const { Client } = require('whatsapp-web.js');
-const fs = require('fs-extra');
+const fs = require('fs');
+const fse = require('fs-extra');
+const SESSION_FILE_PATH = "./session.json";
 
+if (fs.existsSync(SESSION_FILE_PATH)) {
+    sessionCfg = require(SESSION_FILE_PATH);
+  }
 
-const client = new Client({ puppeteer: { headless: true, 
-    args: [
-    '--no-sandbox'
-], 
-}});
+  client = new Client({	  
+    
+    puppeteer: {
+   executablePath: config.chrome_path,
+   headless: true,
+   args: [
+ "--log-level=3",
+ "--no-default-browser-check",
+ "--disable-infobars",
+ "--disable-web-security",
+ "--disable-site-isolation-trials",
+ "--no-experiments",
+ "--ignore-gpu-blacklist",
+ "--ignore-certificate-errors",
+ "--ignore-certificate-errors-spki-list",
+
+ "--disable-extensions",
+ "--disable-default-apps",
+ "--enable-features=NetworkService",
+ "--disable-setuid-sandbox",
+ "--no-sandbox",
+
+ "--no-first-run",
+ "--no-zygote"
+]
+   
+},	      
+session: sessionCfg
+});
+
+client.initialize();
 
 client.on('qr', (qr) => {
     console.log(qr);
 });
 
-client.on('ready', () => {
+client.on("authenticated", session => {
+    console.log('Autentikasi berhasil!');
+    sessionCfg = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function(err) {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
+  
+  client.on("auth_failure", msg => {
+    console.log(
+      'Autentikasi gagal!'
+    );
+    fs.unlink("./session.json", function(err) {
+      if (err) return console.log(err);
+      console.log(
+          'Sesi sudah dihapus, mohon restart!'
+      );
+      process.exit(1);
+    });
+  });
+  
+  client.on("ready", () => {
     console.log('Bot sedang berjalan!');
-});
+  });
+  
 
 client.on('message', async msg => {
     if (msg.body == '!help') {
@@ -22,17 +77,19 @@ client.on('message', async msg => {
 *!mentionall* Untuk mention semua member grup.
 Contoh : !mention absen
 
+*!owner* Untuk mengirim kontak pembuat bot ini.
+
 Group Admin Only :
 *!promote*  Untuk menjadikan member sebagai admin.
 Contoh : !promote @sadbot
+
 *!demote*  Untuk menjadikan admin sebagai member.
 Contoh : !demote @sadbot
+
 *!customwelcome* _pendingggg, will be available later!!!! ;)_
 
 Owner Bot Only :
 *!turnoff*
-
-At *23:30* this bot will be shut down for the safety of this bot.
 `);
     } 
     
@@ -104,4 +161,3 @@ client.on('group_leave', async (notification) => {
     let number = await notification.id.remote;
     client.sendMessage(number, `Selamat tinggal temanku :(`);
 });
-client.initialize();
