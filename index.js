d60534bb4f824b26434ba6d85030105b5c72c10f
nobody <73385158+dikanata@users.noreply.github.com>
2020-12-20 19:49:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const { Client } = require('whatsapp-web.js');
+const fs = require("fs"); 
+const SESSION_FILE_PATH = "./session.json";
+
+if (fs.existsSync(SESSION_FILE_PATH)) {
+    sessionCfg = require(SESSION_FILE_PATH);
+}
 
 const client = new Client({ puppeteer: { headless: true,
     args: [
         "--no-sandbox"
-      ]
-}});
+      ]},
+    session: sessionCfg
+});
 
 client.initialize();
 
