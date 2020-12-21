7f4660f4ef69627bf74f4901299166977e9b772e
nobody <73385158+dikanata@users.noreply.github.com>
2020-12-21 17:39:41 +0700

    Create carbon.js

---
+++ modules/carbon.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+async function mainF(text) {
+
+    var respoimage = await axios.get(`https://carbonnowsh.herokuapp.com/?code=${text.replace(/ /gi,"+")}&theme=darcula&backgroundColor=rgba(36, 75, 115)`, { responseType: 'arraybuffer' }).catch(function(error) {
+        return "error"
+    })
+
+    return ({
+        mimetype: "image/png",
+        data: Buffer.from(respoimage.data).toString('base64'),
+        filename: "carbon"
+    })
+}
+
+module.exports = {
+    mainF
+}
