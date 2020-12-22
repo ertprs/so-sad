9d2b686f55881e0f4a8c344d57f3f088aed749a4
nobody <73385158+dikanata@users.noreply.github.com>
2020-12-22 14:33:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,34 +6,7 @@ const carbon = require('./modules/carbon');
 const fetch = require("node-fetch");  
 const cheerio = require("cheerio");
 const urlencode = require("urlencode");
-const NetworkSpeed = require('network-speed');
 const axios = require("axios");
-
-
-const testNetworkSpeed = new NetworkSpeed();
-
-function getNetworkDownloadSpeed() {
-  const baseUrl = 'http://eu.httpbin.org/stream-bytes/500000';
-  const fileSizeInBytes = 500000;
-  const speed = testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
-  return speed;
-}
-
-function getNetworkUploadSpeed() {
-  const options = {
-    hostname: 'www.google.com',
-    port: 80,
-    path: '/catchers/544b09b4599c1d0200000289',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-  const fileSizeInBytes = 2000000
-  const speed = testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
-  return speed;
-}
-
 
 
 
@@ -415,14 +388,12 @@ Contoh : !mention absen
             }
 
         else if(msg.body == '!speedtest'){
-            const upload = getNetworkUploadSpeed();
-            const download = getNetworkDownloadSpeed();
-
             msg.reply(`*KECEPATAN INTERNET DI SERVER BOT*
-Upload : ${JSON.stringify(upload)}
-Download : ${JSON.stringify(download)}
+
+
 
 `);
         }
+        
 
 });
